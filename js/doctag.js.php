74106af7befaa6d41818b3795658c1e66ef91000
jyhere <jyhere@gmail.com>
2021-06-29 12:35:39 +0200

    Fix tags in ged not properly displayed when using ajax ecm list

--- js/doctag.js.php
+++ js/doctag.js.php
@@ -1,84 +1,93 @@
 <?php
-	if (!defined('NOTOKENRENEWAL')) define('NOTOKENRENEWAL', 1);
+if (!defined('NOTOKENRENEWAL')) {
+    define('NOTOKENRENEWAL', 1);
+}
 
-	if(is_file('../../master.inc.php'))  require('../../master.inc.php');
-	else require('../../../master.inc.php');
+if(is_file('../../master.inc.php')) {
+    require('../../master.inc.php');
+}
+else {
+    require('../../../master.inc.php');
+}
 
-	$langs->load('doctag@doctag');
-	//echo $langs->trans('PreviewOf');
+$langs->load('doctag@doctag');
+//echo $langs->trans('PreviewOf');
 ?>
 
 function docTag_set_link() {
-<?php
-	if(DOL_VERSION>=5) {
-		echo '$("a.documentpreview[href],a.pictopreview[href]")';
-	}
-	else {
-		echo '$("a[href]")';
-	}
-?>.each(function() {
-		var $a = $(this);
-		var url = $a.attr('href');
+    $('.doctag-link').remove();
 
-		if(url.indexOf('document.php?')!=-1 && url.indexOf('action=delete')==-1  && url.indexOf('file=')!=-1) {
-			filename = $a.text();
-			if(filename == '') filename = $a.find('img').attr('alt');
-			var tag64 = window.btoa(url);
-			url = "javascript:docTag_pop('<?php echo dol_buildpath('/doctag/tag.php',1) ?>?tag64="+ tag64 +"','"+filename+"')";
-			if($a.hasClass('pictopreview') && !$a.hasClass('documentpreview')) {
-				link = '<br /><a href="'+url+'" tag64="'+tag64+'"><?php echo img_object($langs->trans('Tagit'),'doctag@doctag').' '.$langs->trans('tag'); ?></a>';
-			}
-			else if($a.parent().is('li')) {
-				link = '';
-			}
-			else{
-				link = '&nbsp;<a href="'+url+'" tag64="'+tag64+'"><?php echo img_object($langs->trans('Tagit'),'doctag@doctag') ?></a>';
-			}
+    <?php if ((float)DOL_VERSION >= 5) : ?>
+    $("a.documentpreview[href],a.pictopreview[href]")
+    <?php else : ?>
+    $("a[href]")
+    <?php endif; ?>
+    .each(function () {
+        var $a = $(this);
+        var url = $a.attr('href');
 
-			if(link.length>0) {
+        if (url.indexOf('document.php?') != -1 && url.indexOf('action=delete') == -1 && url.indexOf('file=') != -1) {
+            filename = $a.text();
+            if (filename == '') {
+                filename = $a.find('img').attr('alt');
+            }
+            var tag64 = window.btoa(url);
+            url = "javascript:docTag_pop('<?= dol_buildpath('/doctag/tag.php', 1) ?>?tag64=" + tag64 + "','" + filename + "')";
+            if ($a.hasClass('pictopreview') && !$a.hasClass('documentpreview')) {
+                link = '<br /><a class="doctag-link" href="' + url + '" tag64="' + tag64 + '"><?= img_object($langs->trans('Tagit'), 'doctag@doctag') . ' ' . $langs->trans('tag'); ?></a>';
+            } else if ($a.parent().is('li')) {
+                link = '';
+            } else {
+                link = '&nbsp;<a class="doctag-link" href="' + url + '" tag64="' + tag64 + '"><?= img_object($langs->trans('Tagit'), 'doctag@doctag') ?></a>';
+            }
 
-				$(this).after(link);
+            if (link.length > 0) {
 
-				$.ajax({
-				    url : "<?php echo dol_buildpath('/doctag/script/interface.php?get=tag64exist&tag64=',1) ?>"+tag64
-				}).done(function(nb_tag) {
-				    if(nb_tag>0) {
-				        $img = $('a[tag64="'+tag64+'"]').find("img");
-				        $img.attr("src","<?php echo  img_picto('', 'object_doctag_blue@doctag', '', false, 1, 1) ?>");
-				        $img.attr("title",nb_tag+" tag(s)");
-				    }
-				});
+                $(this).after(link);
 
-			}
-		}
-
-	});
+                $.ajax({
+                    url: "<?= dol_buildpath('/doctag/script/interface.php?get=tag64exist&tag64=', 1) ?>" + tag64,
+                    global: false
+                }).done(function (nb_tag) {
+                    if (nb_tag > 0) {
+                        $img = $('a[tag64="' + tag64 + '"]').find("img");
+                        $img.attr("src", "<?= img_picto('', 'object_doctag_blue@doctag', '', false, 1, 1) ?>");
+                        $img.attr("title", nb_tag + " tag(s)");
+                    }
+                });
+            }
+        }
+    });
 }
 
 function docTag_pop(url, filename) {
 
-	$('#docTag').remove();
+    $('#docTag').remove();
 
-	if($('#docTag').length==0) {
-		$('body').append('<div id="docTag"><iframe src="#" width="100%" height="100%" allowfullscreen webkitallowfullscreen frameborder="0"></iframe></div>');
-	}
+    if($('#docTag').length==0) {
+        $('body').append('<div id="docTag"><iframe src="#" width="100%" height="100%" allowfullscreen webkitallowfullscreen frameborder="0"></iframe></div>');
+    }
 
-	$('#docTag').dialog({
-		title: "<?php echo $langs->trans('TagsOfThis') ?> "
-		,resize:'auto'
-		,width:800
-		,height:500
-		,modal:true
-		,resizable: true
-		,close:function() {
-			$('#docTag iframe').attr('src', '#');
-		}
-	});
+    $('#docTag').dialog({
+        title: "<?php echo $langs->trans('TagsOfThis') ?> "
+        ,resize:'auto'
+        ,width:800
+        ,height:500
+        ,modal:true
+        ,resizable: true
+        ,close:function() {
+            $('#docTag iframe').attr('src', '#');
+        }
+    });
 
-	$('#docTag iframe').attr('src', url);
+    $('#docTag iframe').attr('src', url);
 
 }
 
 $(document).ready(function() {
-	docTag_set_link();
+    docTag_set_link();
+});
+
+$(document).ajaxComplete(function() {
+    docTag_set_link();
 });
